1adcdb679e5bf27e565823d2d2230605f420b193
Kaixuan Khoo <kk5217@ic.ac.uk>
2021-11-08 01:11:29 +0000

    Day 1 working in complab, only has input from command line in the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include "errors.h"
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    cout << "The files loaded are: " << endl;
+    cout << "Plugboard: " << argv[1] << endl;
+    cout << "Reflector: " << argv[2] << endl;
+    cout << "Rotors (1, 2 and 3): " << argv[3] 
+        << " " << argv[4] << " " << argv[5] << endl;
+    cout << "Rotor position: " << argv[6] << "\n" << endl;
+
+    
 
     return 0;
 }
